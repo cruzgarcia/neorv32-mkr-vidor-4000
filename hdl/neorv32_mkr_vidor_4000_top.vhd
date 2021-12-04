9a5169e6f814b708327f1b54a3bc5dc1d851e81d
Cruz Garcia <cruzgarcia@posteo.de>
2021-12-04 21:55:01 +0100

    - SignalTap removed - Some code formatting

--- hdl/neorv32_mkr_vidor_4000_top.vhd
+++ hdl/neorv32_mkr_vidor_4000_top.vhd
@@ -1,62 +1,76 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use ieee.std_logic_unsigned.all;
+LIBRARY IEEE;
+  USE IEEE.std_logic_1164.ALL;
+  USE IEEE.numeric_std.ALL;
+  USE IEEE.std_logic_unsigned.ALL;
 
-library neorv32;
-use neorv32.neorv32_package.all;
+LIBRARY neorv32;
+  USE neorv32.neorv32_package.ALL;
 
-library sdram_controller;
+LIBRARY sdram_controller;
 
-entity neorv32_mkr_vidor_4000_top is
-  generic (
-    CLOCK_FREQUENCY     : natural := 48_000_000;  -- clock frequency of clk_i in Hz
+ENTITY neorv32_mkr_vidor_4000_top IS
+  GENERIC (
+    CLOCK_FREQUENCY               : NATURAL := 48_000_000;  -- clock frequency of clk_i in Hz
+    -- On-Chip Debugger (OCD) --
+    ON_CHIP_DEBUGGER_EN           : BOOLEAN := TRUE;        -- implement on-chip debugger
+    INT_BOOTLOADER_EN             : BOOLEAN := TRUE;       -- boot configuration: true = boot explicit bootloader; false = boot from int/ext (I)MEM
+    -- RISC-V CPU Extensions --
+    CPU_EXTENSION_RISCV_C         : BOOLEAN := TRUE;        -- implement compressed extension?
+    CPU_EXTENSION_RISCV_M         : BOOLEAN := TRUE;        -- implement mul/div extension?
+    CPU_EXTENSION_RISCV_Zicsr     : BOOLEAN := TRUE;        -- implement CSR system?
+    CPU_EXTENSION_RISCV_Zifencei  : BOOLEAN := TRUE;        -- implement instruction stream sync.?
     -- Internal Instruction memory (IMEM) --
-    MEM_INT_IMEM_EN     : boolean := TRUE;     -- implement processor-internal instruction memory
-    MEM_INT_IMEM_SIZE   : natural := 32*1024;  -- size of processor-internal instruction memory in bytes
+    MEM_INT_IMEM_EN               : BOOLEAN := TRUE;        -- implement processor-internal instruction memory
+    MEM_INT_IMEM_SIZE             : NATURAL := 32*1024;     -- size of processor-internal instruction memory in bytes
     --
-    MEM_INT_DMEM_EN     : boolean := TRUE;    -- implement processor-internal data memory
-    MEM_INT_DMEM_SIZE   : natural := 8*1024;   -- size of processor-internal data memory in bytes
+    MEM_INT_DMEM_EN               : BOOLEAN := TRUE;        -- implement processor-internal data memory
+    MEM_INT_DMEM_SIZE             : NATURAL := 8*1024;      -- size of processor-internal data memory in bytes
     -- External memory interface (WISHBONE)
-    MEM_EXT_EN          : boolean := TRUE;   -- implement external memory bus interface?
-    MEM_EXT_TIMEOUT     : natural := 255;    -- cycles after a pending bus access auto-terminates (0 = disabled)
-    MEM_EXT_PIPE_MODE   : boolean := FALSE;  -- protocol: false=classic/standard wishbone mode, true=pipelined wishbone mode
-    MEM_EXT_BIG_ENDIAN  : boolean := FALSE;  -- byte order: true=big-endian, false=little-endian
-    MEM_EXT_ASYNC_RX    : boolean := FALSE;  -- use register buffer for RX data when false    
-    --
-    IO_SPI_EN           : boolean := true       -- implement serial peripheral interface (SPI)?
+    MEM_EXT_EN                    : BOOLEAN := TRUE;        -- implement external memory bus interface?
+    MEM_EXT_TIMEOUT               : NATURAL := 255;         -- cycles after a pending bus access auto-terminates (0 = disabled)
+    MEM_EXT_PIPE_MODE             : BOOLEAN := FALSE;       -- protocol: false=classic/standard wishbone mode, true=pipelined wishbone mode
+    MEM_EXT_BIG_ENDIAN            : BOOLEAN := FALSE;       -- byte order: true=big-endian, false=little-endian
+    MEM_EXT_ASYNC_RX              : BOOLEAN := FALSE;       -- use register buffer for RX data when false    
+    -- Processor peripherals --
+    IO_SPI_EN                     : BOOLEAN := TRUE;        -- implement serial peripheral interface (SPI)?
+    IO_MTIME_EN                   : BOOLEAN := TRUE         -- implement machine system timer (MTIME)?
   );
-  port (
+  PORT (
     -- Global signals
-    clk_i               : in  std_ulogic;
-    rstn_i              : in  std_ulogic;
+    clk_i               : IN    std_ulogic;
+    rstn_i              : IN    std_ulogic;
+    -- JTAG on-chip debugger interface
+    jtag_tck_i          : IN    std_ulogic;
+    jtag_tdi_i          : IN    std_ulogic;
+    jtag_tdo_o          : OUT   std_ulogic;
+    jtag_tms_i          : IN    std_ulogic;
     -- GPIO
-    gpio_o              : out std_ulogic_vector(7 downto 0);
+    gpio_o              : OUT   std_ulogic_vector(7 downto 0);
     -- UART 0
-    uart0_txd_o         : out std_ulogic;
-    uart0_rxd_i         : in  std_ulogic;
+    uart0_txd_o         : OUT   std_ulogic;
+    uart0_rxd_i         : IN    std_ulogic;
     -- Serial Flash
-    flash_cs_o          : out std_logic;
-    flash_clk_o         : out std_logic;
-    flash_so            : out std_logic;
-    flash_si            : in  std_logic;
-    flash_i02           : out std_logic;
-    flash_i03           : out std_logic;
+    flash_cs_o          : OUT   std_logic;
+    flash_clk_o         : OUT   std_logic;
+    flash_so            : OUT   std_logic;
+    flash_si            : in    std_logic;
+    flash_i02           : OUT   std_logic;
+    flash_i03           : OUT   std_logic;
     -- SDRAM
-    o_sdram_addr        : out   std_logic_vector(11 downto 0);                    -- addr
-    o_sdram_ba          : out   std_logic_vector(1 downto 0);                     -- ba
-    o_sdram_cas_n       : out   std_logic;                                        -- cas_n
-    o_sdram_clk         : out   std_logic;
-    o_sdram_cke         : out   std_logic;                                        -- cke
-    o_sdram_cs_n        : out   std_logic;                                        -- cs_n
-    io_sdram_dq         : inout std_logic_vector(15 downto 0) := (others => 'X'); -- dq
-    o_sdram_dqm         : out   std_logic_vector(1 downto 0);                     -- dqm
-    o_sdram_ras_n       : out   std_logic;                                        -- ras_n
-    o_sdram_we_n        : out   std_logic                                         -- we_n
+    o_sdram_addr        : OUT   std_logic_vector(11 downto 0);
+    o_sdram_ba          : OUT   std_logic_vector(1 downto 0);
+    o_sdram_cas_n       : OUT   std_logic;
+    o_sdram_clk         : OUT   std_logic;
+    o_sdram_cke         : OUT   std_logic;
+    o_sdram_cs_n        : OUT   std_logic;
+    io_sdram_dq         : INOUT std_logic_vector(15 downto 0) := (others => 'X');
+    o_sdram_dqm         : OUT   std_logic_vector(1 downto 0);
+    o_sdram_ras_n       : OUT   std_logic;
+    o_sdram_we_n        : OUT   std_logic
   );
-end entity;
+END ENTITY;
 
-architecture rtl of neorv32_mkr_vidor_4000_top is
+ARCHITECTURE rtl OF neorv32_mkr_vidor_4000_top IS
 
   -- SPI
   constant c_spi_csn_flash_index      : integer := 0;
@@ -118,29 +132,30 @@ architecture rtl of neorv32_mkr_vidor_4000_top is
 		);
 	end component sdram_controller;
 
-begin
+BEGIN
 
-  -- TODO Use a PLL?
+  -- TODO :: Use a PLL
   o_sdram_clk   <= clk_i;
-
-  -- TODO Sync logic for reset to PLL
 
   -- NEORV32
   neorv32_top_inst: neorv32_top
   generic map (
     -- General --
-    CLOCK_FREQUENCY              => CLOCK_FREQUENCY,   -- clock frequency of clk_i in Hz
-    INT_BOOTLOADER_EN            => true,              -- boot configuration: true = boot explicit bootloader; false = boot from int/ext (I)MEM
+    CLOCK_FREQUENCY              => CLOCK_FREQUENCY,
+    INT_BOOTLOADER_EN            => INT_BOOTLOADER_EN,
+    -- On-Chip Debugger (OCD) --
+    ON_CHIP_DEBUGGER_EN          => ON_CHIP_DEBUGGER_EN,
     -- RISC-V CPU Extensions --
-    CPU_EXTENSION_RISCV_C        => true,              -- implement compressed extension?
-    CPU_EXTENSION_RISCV_M        => true,              -- implement mul/div extension?
-    CPU_EXTENSION_RISCV_Zicsr    => true,              -- implement CSR system?
+    CPU_EXTENSION_RISCV_C        => CPU_EXTENSION_RISCV_C,
+    CPU_EXTENSION_RISCV_M        => CPU_EXTENSION_RISCV_M,
+    CPU_EXTENSION_RISCV_Zicsr    => CPU_EXTENSION_RISCV_Zicsr,
+    CPU_EXTENSION_RISCV_Zifencei => CPU_EXTENSION_RISCV_Zifencei,
     -- Internal Instruction memory --
-    MEM_INT_IMEM_EN              => MEM_INT_IMEM_EN,   -- implement processor-internal instruction memory
-    MEM_INT_IMEM_SIZE            => MEM_INT_IMEM_SIZE, -- size of processor-internal instruction memory in bytes
+    MEM_INT_IMEM_EN              => MEM_INT_IMEM_EN,
+    MEM_INT_IMEM_SIZE            => MEM_INT_IMEM_SIZE,
     -- Internal Data memory --
-    MEM_INT_DMEM_EN              => MEM_INT_DMEM_EN,   -- implement processor-internal data memory
-    MEM_INT_DMEM_SIZE            => MEM_INT_DMEM_SIZE, -- size of processor-internal data memory in bytes
+    MEM_INT_DMEM_EN              => MEM_INT_DMEM_EN,
+    MEM_INT_DMEM_SIZE            => MEM_INT_DMEM_SIZE,
     --
     MEM_EXT_EN                   => MEM_EXT_EN,
     MEM_EXT_TIMEOUT              => MEM_EXT_TIMEOUT,
@@ -149,9 +164,9 @@ begin
     MEM_EXT_ASYNC_RX             => MEM_EXT_ASYNC_RX,
     -- Processor peripherals --
     IO_GPIO_EN                   => true,              -- implement general purpose input/output port unit (GPIO)?
-    IO_MTIME_EN                  => true,              -- implement machine system timer (MTIME)?
+    IO_MTIME_EN                  => IO_MTIME_EN,
     IO_UART0_EN                  => true,              -- implement primary universal asynchronous receiver/transmitter (UART0)?
-    IO_TWI_EN                    => FALSE,              -- implement two-wire interface (TWI)?
+    IO_TWI_EN                    => FALSE,             -- implement two-wire interface (TWI)?
     -- Processor peripherals --
     IO_SPI_EN                    => IO_SPI_EN
   )
@@ -159,6 +174,12 @@ begin
     -- Global control --
     clk_i                       => clk_i,
     rstn_i                      => rstn_i,
+    -- JTAG on-chip debugger interface (available if ON_CHIP_DEBUGGER_EN = true) --
+    jtag_trst_i                 => '1',               -- TAP reset (low-active), optional
+    jtag_tck_i                  => jtag_tck_i,
+    jtag_tdi_i                  => jtag_tdi_i,
+    jtag_tdo_o                  => jtag_tdo_o,
+    jtag_tms_i                  => jtag_tms_i,
     -- Wishbone bus interface (available if MEM_EXT_EN = true)
     wb_tag_o                    => wb_tag_o,
     wb_adr_o                    => wb_adr_o,
@@ -192,6 +213,7 @@ begin
   flash_so          <= spi_sdo;
   flash_i02         <= 'Z';
   flash_i03         <= 'Z';
+
   -- SPI CSn multiplexer
   proc_flash_din_mux : process(spi_csn, flash_si, spi_sdi)
   begin
--- syn/constraints/timing.sdc
+++ syn/constraints/timing.sdc
@@ -1,3 +1,3 @@
 
-create_clock -name i_osc_clock -period 21.000 [get_ports {clk_i}]
+create_clock -name i_osc_clock -period 20.833 [get_ports {clk_i}]
 derive_clock_uncertainty
